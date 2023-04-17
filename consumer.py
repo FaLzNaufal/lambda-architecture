4e35b305fb742e4114512abc8a8af0f2d263699d
JevantJedidia <jevantjedidia@gmail.com>
2023-04-17 19:06:42 +0700

    add topic

--- consumer.py
+++ consumer.py
@@ -2,9 +2,33 @@ import json
 from time import sleep
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('socmed',bootstrap_servers=['localhost:9092'])
+consumerfb = KafkaConsumer('facebook',bootstrap_servers=['localhost:9092'])
 
-for message in consumer:
+for message in consumerfb:
+    data = json.loads(message.value)
+    print(data)
+    sleep(10)
+
+
+consumertwt = KafkaConsumer('twitter',bootstrap_servers=['localhost:9092'])
+
+for message in consumertwt:
+    data = json.loads(message.value)
+    print(data)
+    sleep(10)
+
+
+consumeryt = KafkaConsumer('youtube',bootstrap_servers=['localhost:9092'])
+
+for message in consumerfb:
+    data = json.loads(message.value)
+    print(data)
+    sleep(10)
+
+
+consumerig = KafkaConsumer('instagram',bootstrap_servers=['localhost:9092'])
+
+for message in consumerig:
     data = json.loads(message.value)
     print(data)
     sleep(10)
